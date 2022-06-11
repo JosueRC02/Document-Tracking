7a589e3cd4975c27b00802a63d62d673f32ed89a
JosueRC02 <jretanacespedes@gmail.com>
2022-06-11 00:09:40 -0600

    validaciones

--- src/class/SinginValidation.js
+++ src/class/SinginValidation.js
@@ -1,5 +1,16 @@
-const SinginValidation = (values) => {
+const SinginValidation = (codigo_organizacion, password) => {
     let error = {};
+
+    if(!codigo_organizacion){
+        error.codigo_organizacion = "Codigo de la organizacion es requerido."
+    }
+    if(!password){
+        error.password = "Contraseña es requerida."
+    }else if(password.length < 8){
+        error.password = "La contraseña debe tener más de 5 caracteres"
+    }
+
+    return error;
 }
 
 export default SinginValidation;
--- src/components/Singup.jsx
+++ src/components/Singup.jsx
@@ -4,10 +4,12 @@ import logo from '../assets/img/registrar.png';
 import '../assets/css/inicios.css'
 import {Apiurl} from '../services/ApiRest';
 import axios from 'axios';
+import SingupValidation from '../class/SingupValidation';
 
 const SingupForm = () => {
 
    const navigate = useNavigate();
+   const [errors, setErrors] = useState({});
 
     const [nombre_organizacion, setnombre_organizacion] = useState('');
     const [codigo_organizacion, setcodigo_organizacion] = useState('');
@@ -18,6 +20,7 @@ const SingupForm = () => {
       
     const manejadorBtn = e => {
           e.preventDefault();
+          setErrors(SingupValidation(nombre_organizacion, codigo_organizacion, area_negocio, ubicacion, telefono, password));
       }
 
   const registrarse = async () => {
@@ -28,12 +31,9 @@ const SingupForm = () => {
         console.log(response);
         console.log(response.data.status);
         alert("Registrado correctamente");
-        navigate('/Singin')
-        window.location.reload(true);
+        navigate('/')
     } catch (error) {
-        alert("Faltan datos");
         console.log("Error: ", error.response.data);
-        window.location.reload(true);
         return error;
     }
   }
@@ -44,17 +44,29 @@ const SingupForm = () => {
 
             <div className="fadeIn first">
                 <br/><br/>
-            <img src={logo} width = "150px" alt="User Icon" />
+            <img src={logo} style={{width: '150px', marginLeft: '148px'}} alt="User Icon" />
                 <br/><br/>
             </div>
 
             <form onSubmit = {manejadorBtn}>
             <input type="text" className="fadeIn second" name="nombre_organizacion" placeholder="Nombre de la Organizacion" onChange={(e) => setnombre_organizacion(e.target.value)} />
+            {errors.nombre_organizacion && <p className = "error">{errors.nombre_organizacion}</p>}
+
             <input type="text" className="fadeIn second" name="codigo_organizacion" placeholder="Codigo de la Organizacion" onChange={(e) => setcodigo_organizacion(e.target.value)} />
+            {errors.codigo_organizacion && <p className = "error">{errors.codigo_organizacion}</p>}
+
             <input type="text" className="fadeIn second" name="area_negocio" placeholder="Area Negocio de la Organizacion" onChange={(e) => setarea_negocio(e.target.value)} />
+            {errors.area_negocio && <p className = "error">{errors.area_negocio}</p>}
+
             <input type="text" className="fadeIn second" name="ubicacion" placeholder="Ubicacion de la Organizacion" onChange={(e) => setubicacion(e.target.value)} />
+            {errors.ubicacion && <p className = "error">{errors.ubicacion}</p>}
+
             <input type="text" className="fadeIn second" name="telefono" placeholder="Telefono de la Organizacion" onChange={(e) => settelefono(e.target.value)} />
+            {errors.telefono && <p className = "error">{errors.telefono}</p>}
+
             <input type="password" className="fadeIn third" name="password" placeholder="Password" onChange={(e) => setpassword(e.target.value)} />
+            {errors.password && <p className = "error">{errors.password}</p>}
+
             <input type="submit" className="fadeIn fourth" value="Registrarse" onClick={registrarse} />
             </form>
 
