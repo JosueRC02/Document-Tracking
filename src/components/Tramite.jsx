e786c04a6703eecdaa14fc29a09e768864ffed20
Sergio <sergio.villanueva.rios@est.una.ac.cr>
2022-06-28 02:18:23 -0600

    TramiteCompleto

--- src/components/Tramite.jsx
+++ src/components/Tramite.jsx
@@ -1,10 +1,12 @@
 import {MdOutlineMapsHomeWork} from "react-icons/md";
+import {RiDeleteBin6Line} from "react-icons/ri";
 import Tippy from "@tippyjs/react";
 import Sidebar from "./Sidebar";
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import {Apiurl} from '../services/ApiRest';
-import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input} from 'reactstrap';
+import Swal from 'sweetalert2';
 
 import 'tippy.js/dist/tippy.css';
 import '../assets/css/depa.css';
@@ -12,8 +14,90 @@ import '../assets/css/depa.css';
 export default function Tramite(){
   
   const[state,setState] = useState()
+  const[states,setStates] = useState()
   const [list, setList] = useState([]);
 
+
+  const [codigo_tramite, setcodigo_tramite] = useState('');
+  const [tipo_tramite, settipo_tramite] = useState('');
+  const [codigo_departamento, setcodigo_departamento] = useState('');
+
+  const [idTramite, setid] = useState('');
+  const [codigo_tramite2, setcodigo_tramite2] = useState('');
+  const [tipo_tramite2, settipo_tramite2] = useState('');
+  const [codigo_departamento2, setcodigo_departamento2] = useState('');
+
+
+  const registrarTramite = async () => {
+
+    const datos = {codigo_tramite: codigo_tramite, tipo_tramite:tipo_tramite, codigo_departamento:codigo_departamento}
+    try {
+        const response = await axios.post(`${Apiurl}/Tramite`, datos);
+        console.log(response);
+        console.log(response.data.status);
+        Swal.fire({
+            text: 'Datos ingresados correctamente',
+            showConfirmButton: false,
+            icon: 'success',
+            timer: 1500
+          })
+          window.location.reload();
+    } catch (error) {
+        console.log("Error: ", error.response.data);
+        Swal.fire({
+            icon: 'error',
+            text: 'Datos ingresados de manera incorrecta',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        return error;
+    }
+  }
+
+  const updateTramite = async () => {
+
+    const datos = {idTramite:idTramite, codigo_tramite:codigo_tramite2, tipo_tramite:tipo_tramite2, codigo_departamento:codigo_departamento2}
+    try {
+        const response = await axios.put(`${Apiurl}/tramite/${idTramite}`, datos);
+        console.log(response);
+        console.log(response.data.status);
+        Swal.fire({
+            text: 'Datos ingresados correctamente',
+            showConfirmButton: false,
+            icon: 'success',
+            timer: 1500
+          })
+          window.location.reload();
+    } catch (error) {
+        console.log("Error: ", error.response.data);
+        Swal.fire({
+            icon: 'error',
+            text: 'Datos ingresados de manera incorrecta',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        return error;
+    }
+  }
+
+  const deleteTramite = async () => {
+    
+    try {
+        const response = await axios.delete(`${Apiurl}/Tramite/${idTramite}`);
+        console.log(response);
+        console.log(response.data.status);
+        window.location.reload();
+    } catch (error) {
+        console.log("Error: ", error.response.data);
+        Swal.fire({
+            icon: 'error',
+            text: 'Ha ocurrido un problema, vuelva a intentarlo',
+            showConfirmButton: false,
+            timer: 1500
+          })
+        return error;
+    }
+  }
 
   const abrirModal=()=>{
     if(state === false){
@@ -23,9 +107,21 @@ export default function Tramite(){
     }
     
   }
+
+  const abrirModa=()=>{
+    if(states === false){
+      setStates(!states);
+      console.log("entro2");
+    }else{
+      setStates(!states);
+      console.log("entro3");
+    }
+  }
+
+
   useEffect(() => {
     axios({
-      url: `${Apiurl}/getTramite`,
+      url: `${Apiurl}/tramite/getTramite`,
     })
       .then((response) => {
         setList(response.data.data);
@@ -36,7 +132,44 @@ export default function Tramite(){
       });
   }, []);
 
-  const ListaDepartamentos = Object.values(list).map(item=>{
+
+  const Update = (id,codigo,tipo,codigoDep)=>{
+    console.log(id)
+    abrirModa()
+    setid(id)
+    setcodigo_tramite2(codigo)
+    settipo_tramite2(tipo)
+    setcodigo_departamento2(codigoDep)
+  }
+
+  const Delete = (id)=>{
+    console.log(id);
+    setid(id);
+    console.log(idTramite);
+    Swal.fire({
+      title: '¿Seguro?',
+      text: "¿Está seguro que desea eliminar el registro?",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Eliminado!',
+          'El tramite fue eliminado',
+          'Exitoso'
+        )
+        deleteTramite()
+      }
+    })
+    
+  }
+
+
+
+  const ListaTramite = Object.values(list).map(item=>{
     return(
         <>
         <tr>
@@ -45,8 +178,14 @@ export default function Tramite(){
           <td>{item.codigo_departamento}</td>
           <td>
               <Tippy content="Tramites">
-                <button type="button" className="btn btn-info" id="botonD">
+                <button  type="button" className="btn btn-info" name={item._id} onClick={()=>Update(item._id,item.codigo_tramite,item.tipo_tramite,item.codigo_departamento)}>
                 <MdOutlineMapsHomeWork size="23px"/>
+                </button>
+              </Tippy>
+              <>&nbsp;&nbsp;&nbsp;</>
+              <Tippy content="Tramites">
+                <button type="button" className="btn btn-danger" name={item._id} onClick={()=>Delete(item._id)}>
+                <RiDeleteBin6Line size="23px"/>
                 </button>
               </Tippy>
           </td>
@@ -75,37 +214,62 @@ export default function Tramite(){
                         <Button id="AgrD" className="btn btn-info" onClick={() => abrirModal()}>Agregar Tramites</Button>
                         <Modal isOpen = {state} style={modalStyles}>
                           <ModalHeader>
-                            Iniciar Sesión
+                            Registrar Tramite
                           </ModalHeader>
                           <ModalBody>
                             <FormGroup>
-                              <Label for="usuario">Usuario</Label>
-                              <Input type="text" id="usuario" placeholder="Usuario"/> 
+                              <Input  type="text" id="codDepa" placeholder="Codigo Tramite" onChange={(e) => setcodigo_tramite(e.target.value)}/> 
                             </FormGroup>
                             <FormGroup>
-                              <Label for="password">Contraseña</Label>
-                              <Input type="text" id="password"/> 
+                              <Input type="text" id="nombre" placeholder="Tipo Tramite" onChange={(e) => settipo_tramite(e.target.value)}/> 
+                            </FormGroup>
+                            <FormGroup>
+                              <Input type="text" id="correo" placeholder="Codigo Departamento" onChange={(e) => setcodigo_departamento(e.target.value)}/> 
                             </FormGroup>
                           </ModalBody>
 
                           <ModalFooter>
-                              <Button color="primary">Iniciar Sesión</Button>
+                              <Button color="primary" onClick={registrarTramite}>Registrar</Button>
                               <Button color="secondary" onClick={() => abrirModal()}>Cerrar</Button>
+                          </ModalFooter>
+                        </Modal>
+                        <Modal isOpen = {states} style={modalStyles}>
+                          <ModalHeader>
+                            Actualizar Tramite
+                          </ModalHeader>
+                          <ModalBody>
+                          <FormGroup>
+                              <Input type="text" id="id" value={idTramite} onChange={(e) => setid(e.target.value)}/> 
+                            </FormGroup>
+                            <FormGroup>
+                              <Input type="text" id="codDepa" placeholder={codigo_tramite2} onChange={(e) => setcodigo_tramite2(e.target.value)}/> 
+                            </FormGroup>
+                            <FormGroup>
+                              <Input type="text" id="nombre" placeholder={tipo_tramite2} onChange={(e) => settipo_tramite2(e.target.value)}/> 
+                            </FormGroup>
+                            <FormGroup>
+                              <Input type="text" id="correo" placeholder={codigo_departamento2} onChange={(e) => setcodigo_departamento2(e.target.value)}/> 
+                            </FormGroup>
+                          </ModalBody>
+
+                          <ModalFooter>
+                              <Button color="primary" onClick={updateTramite}>Registrar</Button>
+                              <Button color="secondary" onClick={() => abrirModa()}>Cerrar</Button>
                           </ModalFooter>
                         </Modal>
                       </div>
                   </div>
               </div>
-              <table className="table table-bordered">
+              <table className="table table-bordered" id="Trami">
                   <thead>
                       <tr>
-                          <th>Codigo</th>
-                          <th>Tipo_Tramites</th>
+                          <th>Codigo_tramite</th>
+                          <th>tipo_tramite</th>
                           <th>Codigo_departamento</th>
                       </tr>
                   </thead>
                   <tbody>
-                          {ListaDepartamentos} 
+                          {ListaTramite} 
                   </tbody>
               </table>
           </div>
