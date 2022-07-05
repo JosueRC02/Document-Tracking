09170b4d9d97a18558d9b95f2c4e25c9197681d7
JosueRC02 <jretanacespedes@gmail.com>
2022-07-04 18:36:32 -0600

    docs

--- src/components/Documentos.jsx
+++ src/components/Documentos.jsx
@@ -1,46 +1,49 @@
 import Sidebar from "./Sidebar";
 import "../assets/css/Documentos.css"
-import FileUpload from '../class/FileUpload';
-import FileList from '../class/FileList';
 import React, { useState } from 'react';
+import axios from 'axios';
+import {Apiurl} from '../services/ApiRest';
+import { Alert } from "bootstrap";
 
 export default function Documentos(){
 
-    const [files, setFiles] = useState([])
+  const [link_documento, setlink_documento] = useState('');
 
-    const removeFile = (filename) => {
-      setFiles(files.filter(file => file.name !== filename))
+  function guardarArchivo(e) {
+    var file = e.target.files[0] //the file
+    var reader = new FileReader() //this for convert to Base64 
+    reader.readAsDataURL(e.target.files[0]) //start conversion...
+    reader.onload = function (e) { //.. once finished..
+      var rawLog = reader.result.split(',')[1]; //extract only thee file data part
+      var dataSend = { dataReq: { data: rawLog, name: file.name, type: file.type }, fname: "uploadFilesToGoogleDrive" }; //preapre info to send to API
+      fetch('https://script.google.com/macros/s/AKfycbzBr-rSNOj5BOxy2CCs81B7viy4pOOn1AkiVrq_8xFet_aNDZ4odqcjTME23lsiHVIW/exec', //your AppsScript URL
+        { method: "POST", body: JSON.stringify(dataSend) }) //send to Api
+        .then(res => res.json()).then((a) => {
+          alert(`Este es el link del archivo ${a.url}`) //See response
+        }).catch(e => console.log(e)) // Or Error in console
     }
-  
-    return (
-        <>
-        <div className="flex">
-        <Sidebar/>
-        <div className="h-screen flex-1 p-7" id="hp">
-      <div>
-        <div className="title">Upload file</div>
-        <FileUpload files={files} setFiles={setFiles}
-          removeFile={removeFile} />
-        <FileList files={files} removeFile={removeFile} />
-        </div>
-        </div>
-        </div>
-      </>
-    );
-    /*return(
+  }
+
+  async function guardarBD(url) {
+    const datos = {link_documento: link_documento}
+    try {
+        window.open(url, '_blank');
+        await axios.post(`${Apiurl}/Documento`, datos);
+    } catch (error) {
+        console.log("Error: ", error.response.data);
+        return error;
+    }
+  }
+
+
+    return(
         <>
         <div className="flex">
         <Sidebar/>
         <div className="h-screen flex-1 p-7" id="hp">
         <div className="file-upload" id = "drag">
-            <button className="file-upload-btn" type="button" onclick="$('.file-upload-input').trigger( 'click' )">Agregar Archivo</button>
+        <input className="file-upload-btn" type="file" accept="application/pdf"  onChange={(e) => guardarArchivo(e)} />
 
-            <div className="image-upload-wrap">
-                <input className="file-upload-input" type='file' onchange="readURL(this);" accept="image/*" />
-                <div className="drag-text">
-                <h3>Arrastre el archivo aqui</h3>
-                </div>
-            </div>
             <div className="file-upload-content">
                 <img className="file-upload-image" src="#" alt="your image" />
                 <div className="image-title-wrap">
@@ -51,5 +54,5 @@ export default function Documentos(){
         </div>
         </div>
         </>
-    );*/
+    );
 }
